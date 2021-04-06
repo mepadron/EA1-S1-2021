34ad2363ef52899ca904875af3d2efe6866d865b
Miguel Padrron <mikyeduards@gmail.com>
2021-04-06 08:48:24 -0400

    se eplico metodos magicos

--- Classes.php
+++ Classes.php
@@ -1,12 +1,24 @@
 <?php
 
 class Personas {
+
+    public $n = "lolo";
+
+    public function __construct($nombre){
+        echo 'Soy el constructor '. $nombre. " ".$this->n;
+    }
+    
     
     public function dameNombre($nombre){
 
         // dameaApellido($ape);
         return "Hola {$nombre} padron soy profesor";      
     
+    }
+
+    public function __destruct()
+    {
+        echo "se desttruye la class";
     }
 }
 // zmnzxmcnzmxcmzxm
--- index.php
+++ index.php
@@ -12,14 +12,12 @@ include 'Classes.php';
 </head>
 <body>
     <h1>hOLA</h1>
-    <?php
-        dameNombre("Miguel Padron Sarmiento","PADRON 77777777777777");
-    ?>
+    
     <p style="background-color: blue;">
         <?php
-            echo "Tu edad es " , dameaEdad(1968);
-            $p= new Personas();
-            echo $p->dameNombre('Miguel');
+    
+            $p= new Personas('Miguel padron');
+            // echo $p->dameNombre('Miguel');
 
         ?>
     </p>
